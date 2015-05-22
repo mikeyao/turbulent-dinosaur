282cb53b7d2c3ceff66576539d73e6362efbb5b6
Jun(Mike) Yao <yaojunmike@juns-MacBook-Pro-2.local>
2015-05-22 16:32:50 -0700

    Download over 500 skills tagNames from AngelList skills <https://angel.co/skills> Next: read resume.txt and using above skills dictionary, counts occurance of words

---
+++ resumeParse/script.js
@@ -0,0 +1,14 @@
+// Once all skills loaded onto html by pressing 'more' on: <https://angel.co/skills>
+
+// var tags = $(".item-tag a");
+// var skills = [];
+// for (var i = 0; i < tags.length; i++){
+//   var skill = $(tags[i]).text();
+//   skills.push(skill);
+// }
+
+// Take resume as input
+
+/*
+resume.txt 
+*/
